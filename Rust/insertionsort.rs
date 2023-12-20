6d395a5bff855680b70ea1514883718f02991776
Siboniso Nene <sibonisodev@gmail.com>
2023-12-20 10:45:14 -0800

    Implementation of the insertion sort algorithm

---
+++ Rust/insertionsort.rs
@@ -0,0 +1,25 @@
+fn insertion_sort(arr: &mut [i32]) {
+    let n = arr.len();
+
+    for i in 1..n {
+        let key = arr[i];
+        let mut j = i;
+
+        // Move elements of arr[0..i-1] that are greater than key to one position ahead of their current position
+        while j > 0 && arr[j - 1] > key {
+            arr[j] = arr[j - 1];
+            j -= 1;
+        }
+        arr[j] = key;
+    }
+}
+
+fn main() {
+    // Example usage
+    let mut arr = vec![64, 25, 12, 22, 11];
+    println!("Unsorted array: {:?}", arr);
+
+    insertion_sort(&mut arr);
+
+    println!("Sorted array: {:?}", arr);
+}
